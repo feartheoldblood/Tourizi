51158f2739ece2ffcd11aaf3a2e25be26dd467bb
Diego Serrano <diegofabian2002@hotmail.com>
2023-04-13 18:40:23 -0500

    Copiando nuevo Archivo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import LoginPage from './componentes/LoginPage/LoginPage';
+import SeccionProducto from './componentes/LoginPage/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contenedor_login'>
+        <LoginPage/>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/LoginPage/LoginPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./LoginPage.css"; 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function LoginPage() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>TOURIZI</Card.Title>
+        <Card.Text>
+            ¿Olvidó su contraseña?
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default LoginPage;
