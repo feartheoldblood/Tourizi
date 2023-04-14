a1a5da95449625403c146c76b86eedac9f41af23
Diego Serrano <diegofabian2002@hotmail.com>
2023-04-13 23:16:59 -0500

    Terminando componente LoginPage

--- src/componentes/LoginPage/LoginPage.js
+++ src/componentes/LoginPage/LoginPage.js
@@ -5,16 +5,23 @@ import Card from 'react-bootstrap/Card';
 
 function LoginPage() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <div className = "Contenedor-padre">
+    <Card style={{ width: '20rem', height: "16rem",'background' : '#A7CAB9' }}>
       <Card.Body>
-        <Card.Title>TOURIZI</Card.Title>
-        <Card.Text>
-            ¿Olvidó su contraseña?
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title style={{'font-family' : 'Verdana, Geneva, Tahoma, sans-serif'}}>TOURIZI</Card.Title>
+        <br/>
+        <section>
+          <input type="text" name="email" id="email" placeholder="Email"/>
+  
+          <input type="text" name="contraseña" id="contraseña" placeholder="Contraseña"/>
+        </section>
+        <br/>
+        <Button variant="primary" style={{background: '#2C2929', width: '12rem', 'font-family' : 'Verdana, Geneva, Tahoma, sans-serif'}}>INGRESAR</Button>
+        <p>¿Olvido su contraseña?</p>
+
       </Card.Body>
     </Card>
+    </div>
   );
 }
 
