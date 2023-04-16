2758e03b771a4016988bca1e6a8674439a019191
Bryam Galiano <bryan1109@hotmail.es>
2023-04-15 19:41:18 -0500

    Registro para el usuario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LoginPage from './componentes/LoginPage/LoginPage';
 import SeccionProducto from './componentes/LoginPage/LoginPage';
+import UsuarioRegistro from './componentes/UsuarioRegistro/Registro';
+
 
 function App() {
   return (
     <div className="App">
       <div className='contenedor_login'>
-        <LoginPage/>
+        <UsuarioRegistro/>
 
       </div>
     </div>
