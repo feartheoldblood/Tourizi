54e3d45ecfe33ac615ede709e2ed0deeff3ff1a8
Johan Agurto <jagurto2014@gmail.com>
2023-04-20 12:47:28 -0500

    agregando home

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import reportWebVitals from './reportWebVitals';
 import LoginPage from './componentes/LoginPage/LoginPage';
 import UsuarioRegistro from './componentes/UsuarioRegistro/Registro';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home1 from './componentes/Home/Home';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <LoginPage/>}/>
+        
+        <Route path='/' element={ <Home1/>}/>
+        <Route path="/Login " element={ <LoginPage/>}/>
         <Route path="/Registro" element={ <UsuarioRegistro/>}/>
+        
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
